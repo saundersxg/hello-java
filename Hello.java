3cc8f8606c1c7c3bf87e9645267a2d0f08755309
saundersxg <69920484+saundersxg@users.noreply.github.com>
2020-08-19 14:47:20 -0400

    saying hello

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/*******************
+*Hello.java
+*******************
+*XiuQin Saunders
+*8/19/2020
+*CMSC 255-002
+*******************/
+
+public class Hello {
+	
+	public static void main(String[] args) {
+	
+	System.out.println("<XiuQin Saunders>");
+	System.out.println("<Hello Java>");
+	System.out.println("<2020>");
+	System.out.println("<CMSC 255-002>");
+	System.out.println();
+	}
+}
